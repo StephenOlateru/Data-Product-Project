976b940f53ac8edfc13a2fb1f27f48b714f981b6
StephenOlateru <steveolateru@gmail.com>
2015-11-20 09:13:26 +0100

    201115a

--- ui.R
+++ ui.R
@@ -10,28 +10,15 @@ shinyUI(fluidPage(
                        "30%" = ".30",
                        "35%" = ".35"
                        )),
-        # sliderInput("bins",
-                    #            "Number of bins:",
-                    #            min = 1,
-                    #             max = 50,
-                    #             value = 30),
           numericInput("nsales","Net Sales",2000000,step=1000),
           numericInput("cofsales","Cost of Sales",375000,step=1000),
           numericInput("opex","Operating Expense",260000,step=1000),
           numericInput("othinc","Other Income",60000,step=1000),
           numericInput("xgain","Extraordinary Gain/Loss",-15000,step=1000),
           numericInput("intexp","Interest Expense",50000,step=1000)
-          #numericInput("taxes","Taxes",475000)
-
       ),
     
       mainPanel(
-        #plotOutput("distPlot"),
-        #h4("Net Sales"),
-        #verbatimTextOutput("netSales"),
-        
-        #h4("Cost Of Sales"),
-        #verbatimTextOutput("costOfSales"),
         
         h5("Gross Income"),
         verbatimTextOutput("grossIncome"),
